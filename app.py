8bed4df413425c364ef34d767a140b2543e6803b
Jeff Fitzsimmons <jeff@crsvrlab.com>
2025-01-28 00:47:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ transformed_data = transform_data(raw_data)
 st.title("Geek Match")
 
 st.write("Enter your **Top 10** board games in order of preference (1 = most favorite).")
-st.write("We'll compare them against our dataset of almost 150 Top 10 liists and show your best matches.")
+st.write("We'll compare them against our dataset of almost 150  board game reviewers' and pundits "Best of 2024" lists and show your best matches.")
 st.write("Based on the 2024 dataset compiled by BGG user vitus979 ")
 user_games_input = []
 for i in range(1, 11):
