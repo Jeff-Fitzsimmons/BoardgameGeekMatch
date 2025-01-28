9bb4ef9c26be947256e51b400b93ae3dcaf17f28
Jeff Fitzsimmons <jeff@crsvrlab.com>
2025-01-28 01:24:44 -0600

    Update app.py
    updated description text on input form

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ st.title("Geek Match")
 st.write("Based on the 2024 dataset compiled by BGG user vitus979 ")
 
 st.write("Enter your **Top 10** board games in order of preference (1 = most favorite).")
-st.write("We'll compare them against our dataset of nearly 150 board game reviewers 'Best of 2024' lists and show the best matches.")
+st.write("We'll compare them against our dataset of nearly 150 board game reviewers 'Best of 2024' lists and show the ones that most closely match your tastes.")
 
 
 user_games_input = []
